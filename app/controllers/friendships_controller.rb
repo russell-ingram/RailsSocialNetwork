64783cd861e2dd8eddcc3ff96e84e433ac8bdb0c
Russell Ingram <russell.ingram@uicentric.com>
2015-04-23 16:29:38 -0400

    connections without acceptance

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,14 +1,42 @@
 class FriendshipsController < ApplicationController
 
+  before_filter :authenticate_user!
+
+  def new
+    if params[:friend_id]
+      @friend = User.find(params[:friend_id])
+      @friendship = current_user.friendships.new(friend: @friend)
+    else
+      flash[:error] = "Friend required"
+    end
+
+  end
+
+
   def create
-    @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
-    if @friendship.save
-      flash[:notice] = "Added friend."
+    puts "create PARAMS:"
+    puts params
+
+    if params[:friendship] && params[:friendship].has_key?(:friend_id)
+      @friend = User.find(params[:friendship][:friend_id])
+      @friendship = current_user.friendships.new(friend: @friend)
+      @friendship.save
       redirect_to '/connections'
     else
-      flash[:notice] = "Unable to add friend."
+      flash[:error] = "Friend required"
       redirect_to '/search'
     end
+
+
+  #   @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
+  #   if @friendship.save
+  #     flash[:notice] = "Added friend."
+  #     redirect_to '/connections'
+  #   else
+  #     flash[:notice] = "Unable to add friend."
+  #     redirect_to '/search'
+  #   end
+
   end
 
 
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,6 +3,4 @@ class Friendship < ActiveRecord::Base
   belongs_to :user
   belongs_to :friend, :class_name => 'User', foreign_key: 'friend_id'
 
-  attr_accessible :user, :friend
-
 end
