e14393161b10bbaed89c5aa8b343bf73431817c7
Andy <andy@Andys-MacBook-Air.local>
2015-10-01 11:54:06 -0400

    fix: code missing

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,17 @@
 module StaticPagesHelper
+
+	def connection_img(rec)
+
+		if rec.profile_pic_url?
+
+			rec.profile_pic_url
+
+		else
+
+			image_path('avatar-icons/avatar_0'+rand(1..9).to_s+'.svg')
+
+		end
+
+	end
+
 end
