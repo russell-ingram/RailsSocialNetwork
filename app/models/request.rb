e980424fa6d8228c4062a63bba6d85ef61a11b79
Russell Ingram <russell.ingram@uicentric.com>
2015-11-03 10:08:03 -0500

    fixed linked in on request

--- app/models/request.rb
+++ app/models/request.rb
@@ -2,6 +2,6 @@ class Request < ActiveRecord::Base
   def parse_linkedin
     full_link = self.linked_in
     a = full_link.slice! "https://www.linkedin.com/pub/"
-    return full_link.split('/')[0]
+    return full_link.split('/')[1]
   end
 end
