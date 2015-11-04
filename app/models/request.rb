70b6805e48e2da4cda1f51120c406a45d5f3097b
Russell Ingram <russell.ingram@uicentric.com>
2015-11-04 11:13:04 -0500

    making request linkedin ui better

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,8 +1,12 @@
 class Request < ActiveRecord::Base
   def parse_linkedin
     full_link = self.linked_in
-    a = full_link.slice! "https://www.linkedin.com/pub/"
-    return full_link.split('/')[1]
+    a = full_link.slice! "https://www.linkedin.com/"
+    if !full_link.split('/')[1].nil?
+      return full_link.split('/')[1]
+    else
+      return ""
+    end
   end
 end
 
