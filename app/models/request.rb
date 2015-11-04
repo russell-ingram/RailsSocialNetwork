17c1626be4a5f927bee9a63f5eb767d05e4887b9
Russell Ingram <russell.ingram@uicentric.com>
2015-11-04 11:02:53 -0500

    undoing last break

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,10 +1,10 @@
 class Request < ActiveRecord::Base
   def parse_linkedin
     full_link = self.linked_in
-    a = full_link.slice! "https://www.linkedin.com/"
+    a = full_link.slice! "https://www.linkedin.com/pub/"
     return full_link.split('/')[1]
   end
 end
 
 
-https://www.linkedin.com/in/sidnyginsberg
+# https://www.linkedin.com/in/sidnyginsberg
